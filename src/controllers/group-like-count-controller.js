628567c3d2faa92624a584c93e5af1c2eb04c8b6
kimjs <kimjs910522@gmail.com>
2025-11-07 11:13:27 +0900

    refactor: try-catch 블록 추가 style   : 변수명 수정 fix     : 감소 로직에서의 버그(마이너스 에러) 수정

--- src/controllers/group-like-count-controller.js
+++ src/controllers/group-like-count-controller.js
@@ -1,63 +1,71 @@
-import { PrismaClient } from "@prisma/client";
+import prisma from "../utils/prisma.js";
 import { NotFoundError } from "../middlewares/error-handler.js";
 
-const prisma = new PrismaClient();
+class GroupLikeCount {
+  async groupLikeCountUp(req, res, next) {
+    try {
+      const groupId = Number(req.params.id);
 
-class GroupLikeCount{
-  async groupLikeCountUp(req, res) {
+      const findGroup = await prisma.group.findUnique({
+        where: { id: groupId },
+      });
 
-    const ID = Number(req.params.id);
+      if (!findGroup) {
+        throw new NotFoundError("그룹을 찾을 수 없습니다");
+      }
 
-    const findGroup = await prisma.group.findUnique({
-      where: { id: ID },
-    });
+      const likeCountUp = await prisma.group.update({
+        where: { id: groupId },
+        data: {
+          likeCount: { increment: 1 },
+        },
+      });
 
-    if (!findGroup) {
-      throw new NotFoundError("그룹을 찾을 수 없습니다");
+      res.status(200).json({
+        message: "그룹 추천 성공",
+        data: {
+          id: likeCountUp.id,
+          likeCount: likeCountUp.likeCount,
+        },
+      });
+    } catch (error) {
+      next(error);
     }
-
-    const likeCountUp = await prisma.group.update({
-      where: { id: ID },
-      data: {
-        likeCount: { increment: 1 },
-      },
-    });
-
-    res.status(200).json({
-      message: "그룹 추천 성공",
-      data: {
-        id: likeCountUp.id,
-        likeCount: likeCountUp.likeCount,
-      },
-    });
   }
 
-  async groupLikeCountDown(req, res) {
+  async groupLikeCountDown(req, res, next) {
+    try {
+      const groupId = Number(req.params.id);
 
-    const id = Number(req.params.id);
+      const findGroup = await prisma.group.findUnique({
+        where: { id: groupId },
+      });
 
-    const findGroup = await prisma.group.findUnique({
-      where: { id },
-    });
+      if (!findGroup) {
+        throw new NotFoundError("그룹을 찾을 수 없습니다");
+      }
 
-    if (!findGroup) {
-      throw new NotFoundError("그룹을 찾을 수 없습니다");
+      if (findGroup.likeCount < 1) {
+        throw new NotFoundError("더 이상 감소할 수 없습니다");
+      }
+
+      const likeCountDown = await prisma.group.update({
+        where: { id: groupId },
+        data: {
+          likeCount: { decrement: 1 },
+        },
+      });
+
+      res.status(200).json({
+        message: "그룹 추천 취소 성공",
+        data: {
+          id: likeCountDown.id,
+          likeCount: likeCountDown.likeCount,
+        },
+      });
+    } catch (error) {
+      next(error);
     }
-
-    const likeCountDown = await prisma.group.update({
-      where: { id },
-      data: {
-        likeCount: { decrement: 1 },
-      },
-    });
-
-    res.status(200).json({
-      message: "그룹 추천 취소 성공",
-      data: {
-        id: likeCountDown.id,
-        likeCount: likeCountDown.likeCount,
-      },
-    });
   }
 }
 
