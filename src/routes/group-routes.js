053d44c389e65208f5ad9000c07706477d3318ee
shpocher <shpocher@gmail.com>
2025-11-17 14:25:01 +0900

    그룹 좋아요 컨트롤러
    그룹 좋아요, 취소 컨트롤러 라우트 등록

--- src/routes/group-routes.js
+++ src/routes/group-routes.js
@@ -3,6 +3,7 @@ import groupController from '../controllers/group-controller.js';
 
 // 이미지 관련 추가 작업
 import { uploadMulti } from '../middlewares/upload.js';
+import groupLikeCount from '../controllers/group-like-count-controller.js';
 
 const groupRouter = express.Router();
 
@@ -11,8 +12,8 @@ groupRouter
   .get('/:id', groupController.getGroupDetail)
   .post('/', uploadMulti, groupController.createGroup) //img 업로드 미들웨어 추가
   .delete('/:id', groupController.deleteGroup)
-  .patch('/:id', uploadMulti, groupController.patchGroup); //img 업로드 미들웨어 추가
-  .post('/:id/likes', groupLikeCount.groupLikeCountUp);
+  .patch('/:id', uploadMulti, groupController.patchGroup) //img 업로드 미들웨어 추가
+  .post('/:id/likes', groupLikeCount.groupLikeCountUp)
   .delete('/:id/likes', groupLikeCount.groupLikeCountDown);
 
 export default groupRouter;
